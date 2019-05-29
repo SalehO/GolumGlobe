d810d592ccfc1eb97271c1e3085848a26a4d1f5d
Omar S <33537398+SalehO@users.noreply.github.com>
2019-05-29 16:29:32 -0700

    got rid of time limit

--- docs/map_temp.py
+++ docs/map_temp.py
@@ -314,8 +314,6 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 
 
                   </DrawingDecorator>
-                  <ServerQuitFromTimeUp timeLimitMs="3000000"/>
-                  <ServerQuitWhenAnyAgentFinishes/>
                 </ServerHandlers>
               </ServerSection>
               
